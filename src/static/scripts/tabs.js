876b8c6da375b963f550add1513ea014f03e1a5b
ruixin__ <61482150+NR721@users.noreply.github.com>
2025-03-10 11:47:07 +0800

    Explanation on ML process

---
+++ src/static/scripts/tabs.js
@@ -0,0 +1,19 @@
+function openTab(evt, tabName) {
+    let i, tabcontent, tablinks;
+
+    // Hide all tab content
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove "active" class from all tab buttons
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the selected tab content and mark button as active
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
